de771db9d16191f4d8639857d72c5882954a8c8a
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-09-28 12:54:53 +0330

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ function App() {
 
   return (
     <>
-      <div className='bg-purple-500 w-6/12 h-32 text-white'>hello react</div>
+      <div className='bg-purple-500 w-6/12 h-36 text-white border-4 border-yellow-400 rounded-lg'><p className='p-5 bg-gray-600 text-xs text-center mx-3 tracking-tight leading-loose'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat totam eius unde quidem officia, <span className='font-bold text-red-900 underline capitalize'>vel illum laborum sapiente</span>, qui maiores ratione quasi dolores. Soluta, delectus quod ab beatae aut dolor!</p>
+      <button className="bg-green-400 m-3 p-3 border-2 border-red-500 rounded-sm">Submit</button>
+      </div>
+      <div className="h-screen flex flex-wrap">
+            <div className="flex w-1/3 h-1/3">
+                <div className="bg-teal-100 m-2 w-full flex justify-center items-center">A</div>
+            </div>
+            <div className="flex w-1/3 h-1/3">
+                <div className="bg-teal-300 m-2 w-full flex justify-center items-center">B</div>
+            </div>
+            <div className="flex w-1/3 h-1/3">
+                <div className="bg-teal-500 m-2 w-full flex justify-center items-center">C</div>
+            </div>
+            </div>
     </>
   )
 }
